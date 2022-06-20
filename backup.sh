dbd4d444a01f7edee5e26b20cb085208d96f9e0d
Jaedo Aum <icarus096@gmail.com>
2022-06-20 12:46:30 +0900

    2022-06-20 12:46 Backup

--- backup.sh
+++ backup.sh
@@ -4,12 +4,12 @@ DOTFILES=$HOME/dotfiles
 
 cd "$(dirname "${BASH_SOURCE}")";
 
-read -p "홈폴더의 설정파일들을 백업하시겠습니까? (y/n) " -n 1;
-echo "";
-if ! [[ $REPLY =~ ^[Yy]$ ]]; then
-    echo "=> 취소 되었습니다."
-    exit 1
-fi;
+#read -p "홈폴더의 설정파일들을 백업하시겠습니까? (y/n) " -n 1;
+#echo "";
+#if ! [[ $REPLY =~ ^[Yy]$ ]]; then
+#    echo "=> 취소 되었습니다."
+#    exit 1
+#fi;
 
 echo
 echo ------------------------------------------------------------------------------
@@ -27,7 +27,7 @@ for name in gitignore gitalias zshrc; do
         exit -1
     fi
 done
-
+echo -e "Check Complete!\n"
 
 #------------------------------------------------------------------------------
 # Install executables and libraries
@@ -35,18 +35,20 @@ done
 # Brewfile 복구 -> brew bundle --file=${DOTFILES}/Brewfile
 #------------------------------------------------------------------------------
 brew bundle dump -f
+echo -e "brew bundle Complete!\n"
 
 
 # iterm2 config file
-echo "com.googlecode.iterm2.plist PASSED!"
+echo -e "com.googlecode.iterm2.plist PASSED!\n"
 #cp ~/Library/Preferences/com.googlecode.iterm2.plist $DOTFILES
 
 
 # dotfiles push
 if [[ $(git status --porcelain) ]]; then
-    git add --all 
-    git commit -m "$(date "+%Y-%m-%d %H:%M") Backup"
-    git push
+  git add --all 
+  git commit -m "$(date "+%Y-%m-%d %H:%M") Backup"
+  git push
+  echo -e "github push Complete!\n"
 else
     echo "git이 최신 상태입니다."
 fi
@@ -60,13 +62,15 @@ fi
 
 # gpg key backup (bitwarden - gpg-password)
 # 필요시 직접수행: gpg export -> bw create item
+echo -e "hammerspoon backup PASSED!\n"
 
 # .ssh folder backup (gpg->bitwarden - bitwarden-password) 
 # 필요시 아래 스크립트 직접수행
 # source ~/dotfiles/function_bitwarden.sh && push_folder ~/.ssh
-
+echo -e ".ssh backup PASSED!\n"
 
 # icloud Documents 동기화폴더에도 복사해 주자. 
 rsync -avh $DOTFILES ~/Documents/ --exclude .git --exclude log/ --delete
+echo -e "rsync to icloud documents backup Complete!\n"
 
 echo "$(date "+%Y-%m-%d %H:%M") Backup to https://github.com/ic4r/dotfiles Complete!"
